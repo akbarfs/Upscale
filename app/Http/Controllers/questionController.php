2c3d119016b25689397d56551d364674a559a90b
Imam Setiawan <imam.unsika@gmail.com>
2020-05-07 00:40:18 +0700

    no message

--- app/Http/Controllers/questionController.php
+++ app/Http/Controllers/questionController.php
@@ -35,17 +35,23 @@ class questionController extends Controller
 
     public function create($id)
     {
-        $inquiry = Inquiry::find($id); // id 1 
+        $inquiry = Inquiry::find($id);
         return view('admin.questionanswer.question',compact('inquiry'));
     }
 
     public function storeQuestion(Request $request)
     {
+        $cek = InquiryQuestion::latest('sort')->first();
+        if ($cek == null)
+        { $sort = 0; }
+        else
+        { $sort= $cek->sort; }
         $id = InquiryQuestion::create ([
             'inquiry_id' => $request->inquiry_id,
             'question' => $request->question,
             'description' => $request->description, 
-            'type_option' => $request->type_option
+            'type_option' => $request->type_option,
+            'sort' => $sort+1
         ]);
         
         $array = $request->option;
