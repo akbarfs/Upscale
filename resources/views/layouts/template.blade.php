7fd31c056641c70a9e4f0e9ef8b9c4110ada946c
dodiprakoso <imedia.inf@gmail.com>
2020-05-16 21:03:38 +0700

    widget chat

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -26,16 +26,29 @@
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
     <!-- Start of HubSpot Embed Code -->
-      <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/7727872.js"></script>
+     <!--  <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/7727872.js"></script> -->
     <!-- End of HubSpot Embed Code -->
 
     @yield('top-asset')
 
 </head>
 <body>
-    <!-- Start of HubSpot Embed Code -->
-  <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/7727872.js"></script>
-<!-- End of HubSpot Embed Code -->
+
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5ebfe22d967ae56c521a60eb/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
+
+    
     <style>
 
         <!-- widget date picker -->
