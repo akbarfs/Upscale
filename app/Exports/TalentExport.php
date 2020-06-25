5415116f289a5372ec6a19095def769f488e79ef
WillyGurning <willygurning17@gmail.com>
2020-06-25 08:59:23 +0700

    second push

--- app/Exports/TalentExport.php
+++ app/Exports/TalentExport.php
@@ -4,14 +4,27 @@ namespace App\Exports;
 
 use App\Models\Talent;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
-class TalentExport implements FromCollection
+
+class TalentExport implements FromCollection, WithHeadings, ShouldAutoSize
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return Talent::paginate(50);
+        return Talent::paginate(20);
+    }
+    public function headings(): array
+    {
+        return [
+            'No',
+            'Name',
+            'Email',
+            'Created at',
+            'Updated at'
+        ];
     }
 }
