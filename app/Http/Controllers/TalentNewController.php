08fd71e80647a7eadd9504bc6cb44b65ca5ad834
GradyKopi <60635492+GradyKopi@users.noreply.github.com>
2020-06-25 16:25:59 +0700

    Masih belum 1 1

--- app/Http/Controllers/TalentNewController.php
+++ app/Http/Controllers/TalentNewController.php
@@ -17,8 +17,8 @@ class TalentNewController extends Controller
 
     public function show()
     {
-    	
-    	return view('admin.talent.home');
+
+        return view('admin.talent.home');
     }
 
     function mail($id)
@@ -29,65 +29,51 @@ class TalentNewController extends Controller
     function mailSend()
     {
         $recipients = [
-            "dodi@upscale.id",
-            "upscale.campaign13@gmail.com",
-			"upscale.campaign14@gmail.com",
-			"upscale.campaign15@gmail.com",
-			"upscale.campaign16@gmail.com",
-			"upscale.campaign17@gmail.com",
-			"upscale.campaign18@gmail.com",
-			"upscale.campaign19@gmail.com",
-			"upscale.campaign20@gmail.com",
-			"upscale.campaign21@gmail.com",
-			"upscale.campaign22@gmail.com",
-			"upscale.campaign23@gmail.com",
-			"upscale.campaign24@gmail.com",
-			"upscale.campaign25@gmail.com",
-			"upscale.campaign26@gmail.com",
-			"upscale.campaign27@gmail.com",
-			"upscale.campaign28@gmail.com",
-			"upscale.campaign29@gmail.com",
-			"upscale.campaign30@gmail.com",
-			"upscale.campaign31@gmail.com",
-			"upscale.campaign32@gmail.com",
-			"upscale.campaign33@gmail.com"
+            ['email' => 'upscale.campaign34@gmail.com'], 
+            ['email' => 'upscale.campaign13@gmail.com'], 
+            ['email' => 'grady.sianturi13@gmail.com']
         ];
         Mail::to($recipients)->send(new UpscaleEmail());
 
-        echo "coba send email lewat sini"; 
+        echo "coba send email lewat sini";
     }
 
     public function paginate_data(Request $request)
     {
         // if ($request->ajax()) {
 
-            $data = Talent::select(DB::raw("*, users.email as member_email"));
+        $data = Talent::select(DB::raw("*, users.email as member_email"));
 
-            $data->join("users","talent.user_id","=","users.id","LEFT");
+        $data->join("users", "talent.user_id", "=", "users.id", "LEFT");
 
-            if ( $request->talent_name ) {$data->where("talent_name","LIKE","%".$request->talent_name."%"); }
-            if ( $request->talent_phone ) {$data->where("talent_phone","LIKE","%".$request->talent_phone."%"); }
-            if ( $request->talent_email ) {$data->where("talent_email","LIKE","%".$request->talent_email."%"); }
-            if ( $request->talent_address ) {$data->where("talent_address","LIKE","%".$request->talent_address."%"); }
+        if ($request->talent_name) {
+            $data->where("talent_name", "LIKE", "%" . $request->talent_name . "%");
+        }
+        if ($request->talent_phone) {
+            $data->where("talent_phone", "LIKE", "%" . $request->talent_phone . "%");
+        }
+        if ($request->talent_email) {
+            $data->where("talent_email", "LIKE", "%" . $request->talent_email . "%");
+        }
+        if ($request->talent_address) {
+            $data->where("talent_address", "LIKE", "%" . $request->talent_address . "%");
+        }
 
-            if ( $request->status_member == "member" )
-            {
-                $data->where("users.email","!=","");
-            }
+        if ($request->status_member == "member") {
+            $data->where("users.email", "!=", "");
+        }
 
-            if ( $request->status_member == "non-member" )
-            {
-                $data->where("users.email","=",null);
-            }
+        if ($request->status_member == "non-member") {
+            $data->where("users.email", "=", null);
+        }
 
-            
 
-            $data->orderBy("talent_id","DESC");
 
-            $data = $data->paginate(10);
+        $data->orderBy("talent_id", "DESC");
 
-            return view('admin.talent.table',compact('data'))->render();
+        $data = $data->paginate(10);
+
+        return view('admin.talent.table', compact('data'))->render();
         // }
     }
-
 }
