1d2d1c685d68ab1afb27320c34f5392306f821a9
WillyGurning <willygurning17@gmail.com>
2020-06-30 16:48:20 +0700

    export commit

--- app/Exports/TalentExport.php
+++ app/Exports/TalentExport.php
@@ -2,7 +2,7 @@
 
 namespace App\Exports;
 
-
+use App\Models\Talent;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
@@ -10,15 +10,69 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 
-
-class TalentExport implements FromCollection, ShouldAutoSize, WithHeadings
+class TalentExport implements FromCollection, ShouldAutoSize,WithHeadings
 { 
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-            $data = Talent::select(DB::raw("talent.talent_name, talent.talent_email,  users.created_at as member_date"));
+            $select  = "talent.talent_name";
+            
+            if ( isset($_GET['contact']) && $_GET['contact'] == 'on' ) 
+            {
+                $select = $select .",talent.talent_email,talent.talent_phone"; 
+            }
+
+            if (isset($_GET['member_date']) && $_GET['member_date'] == 'on')
+            {
+                $select = $select .", users.created_at as member_date";
+            }
+
+            if (isset($_GET['ready_jogja']) && $_GET['ready_jogja'] == 'on')
+            {
+                $select = $select .",talent.talent_onsite_jogja";
+            }
+
+            if (isset($_GET['ready_jakarta']) && $_GET['ready_jakarta'] == 'on')
+            {
+                $select = $select .",talent.talent_onsite_jakarta";
+            }
+
+            if (isset($_GET['isa']) && $_GET['isa'] == 'on')
+            {
+                $select = $select .",talent.talent_isa";
+            }
+
+            if (isset($_GET['created']) && $_GET['created'] == 'on')
+            {
+                $select = $select .",talent.talent_created_date";
+            }
+
+            if (isset($_GET['skill']) && $_GET['skill'] == 'on')
+            {
+                $select = $select .",talent.talent_skill";
+            }
+            
+            if (isset($_GET['date_ready']) && $_GET['date_ready'] == 'on')
+            {
+                $select = $select .",talent.talent_date_ready";
+            }
+
+            if (isset($_GET['active']) && $_GET['active'] == 'on')
+            {
+                $select = $select .",talent.talent_last_active  ";
+            }
+
+
+            //ok lengkapin y , hati2 masalah komanya ,  krena kan penggabungan string
+            //klo digabung komanya lupa nnti jadinya "talent_name talentEmail, talent_Phone
+            //klo kosong gt jd error , gmn udah bisa y ? komanya yg benar seperti yg di contack ya pak?
+            //y tergntung talent.talent_name,talent.talent_email,talent.talent_phone, users.created_at as member_date
+            //nah klo digabungkan bener ga ada yg kosong , paham ga ? varubale $select cm dilanjutin terus kebawah
+            //gmn udah paham ? paham pak
+
+            $data = Talent::select(DB::raw($select)); 
 
             $data->join("users","talent.user_id","=","users.id","LEFT");
 
@@ -57,10 +111,15 @@ class TalentExport implements FromCollection, ShouldAutoSize, WithHeadings
 
     public function headings(): array
     {
-        return [
+        $select  = "talent.talent_name";
+        if ( isset($_GET['contact']) && $_GET['contact'] == 'on' ) 
+        {
+              return [
             'Nama',
             'Email', 
         ];
+        }
+   
     }
     
 }
