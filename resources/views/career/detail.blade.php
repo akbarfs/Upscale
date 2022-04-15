b1acb00dfb7a1a1cabd9f562b0face0cc55f4836
DESKTOP-BODJNPD\dodi pc <imedia.inf@gmail.com>
2022-04-15 23:28:35 +0700

    no message

--- resources/views/career/detail.blade.php
+++ resources/views/career/detail.blade.php
@@ -6,6 +6,128 @@
 
 
 <main>
+
+    <section class="section-video light">
+        <video autoplay loop muted playsinline poster="{{url('template/upscale/media/video-poster-3.jpg')}}">
+            <source src="{{url('template/upscale/media/repeat-medium.mp4')}}" type="video/mp4">
+        </video>
+        <div class="container" style="padding-bottom: 100px;">
+            <hr class="space-lg hidden-sm" />
+            <div class="row align-items-center" data-anima="fade-in" data-time="1000">
+                <div class="col-lg-6" style="font-size: 25px">
+                    <h1 class="slide slide-title1">
+                         {{lang('Apply as our talent.','Daftar sebagai talent.')}}
+                    </h1>
+                    
+                    <!-- <h2 class="slide-title2">
+                        {!!lang(
+                            
+                            'Just focus on scaling your business and let us do the "Talent" things.',
+
+                            'Kami membantu meng-handle semua urusan "talent", agar anda dapat fokus pada pengembangan bisnis.'
+
+                        )!!}
+                    </h2> -->
+
+                    <p class="slide-title3" style="margin-bottom: 20px">
+                    {!!lang(
+                        'Hire software developers, designers, product manager, finance or administrative team from our exclusive networks. Our talents are available for both on-site or work remotely from our office.<br /><br />Choose from the Full-time or Freelance contract based on your business need.',
+                        'Hire software developers, designers, product manager, finance, atau administratif team dari eksklusif network kami. Talent kami bisa bekerja secara remote maupun on-site.<br /><br />Kontrak full-time maupun freelance yang disesuaikan dengan kebutuhan bisnis Anda.'
+                    )!!}
+                    </p>
+                        
+                </div>
+                <div class="col-lg-6 align-center align-center-md" data-anima="fade-in" data-time="1000">
+
+                        <a href="#" class="btn btn-circle btn-sm join_community" data-target="#registerTalent" data-toggle="modal" style="border-color: #fff !important; color: #fff !important;">
+                            {!!lang('Join as Talent','Daftar Sebagai Talent')!!}
+                        </a>
+
+                </div>
+            </div>
+            <hr class="space-lg hidden-sm" />
+        </div>
+    </section>
+
+    <section id="features" class="section-base section-color section-top-overflow" style="background: #37517E">
+        <div class="container" style="padding-top: 0">
+            <div class="grid-list" data-columns="4" data-columns-md="2" data-columns-xs="1">
+
+                
+                <div class="grid-box">
+
+                    <div class="grid-item">
+                        <div class="cnt-box cnt-box-top-icon boxed">
+                            <div class="caption">
+                                <h2>Dedicated Team</h2>
+                                <p>
+                                    {{lang('Hire an outsource talented team to help scale up your business.','Hire talent sebagai karyawan outsource.')}}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="grid-item">
+                        <div class="cnt-box cnt-box-top-icon boxed">
+                            <div class="caption">
+                                <h2>Freelance Team</h2>
+                                <p>
+                                    {{lang('Part-time team that work based on the man-hour rate.','Hire talent freelance berbasis man-hour.')}}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="grid-item">
+                        <div class="cnt-box cnt-box-top-icon boxed">
+                            <div class="caption">
+                                <h2>Head Hunter</h2>
+                                <p>
+                                    {{lang('Make your recruitment process faster and hassle-free.','Hire talent sebagai karyawan internal anda.')}}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="grid-item">
+                        <div class="cnt-box cnt-box-top-icon boxed">
+                            <div class="caption">
+                                <h2>Project-Based</h2>
+                                <p>
+                                    {{lang('A legacy project-based team to finish up your project.','Hire vendor / freelance berbasis project.')}}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr class="space-lg" />
+            
+        </div>
+    </section>
+
+    <section class="section-base section-color section-top-overflow" style="background: #37517E">
+        <div class="container" style="padding-top: 0">
+            <ul class="slider" data-options="type:carousel,arrows:false,nav:false,perView:5,perViewMd:3,perViewXs:2,gap:80">
+                <li  class="tooltipx">
+                    <img src="{{url('template/upscale/media/logos/white/logo-1.png')}}" alt=""  />
+                </li>
+                <li class="tooltipx">
+                    <img src="{{url('template/upscale/media/logos/white/logo-2.png')}}" alt="" />
+                </li>
+                <li class="tooltipx">
+                    <img src="{{url('template/upscale/media/logos/white/logo-3.png')}}" alt="" />
+                </li>
+                <li class="tooltipx">
+                    <img src="{{url('template/upscale/media/logos/white/logo-4.png')}}" alt="" />
+                </li>
+                <li class="tooltipx">
+                    <img src="{{url('template/upscale/media/logos/white/logo-5.png')}}" alt="" />
+                </li>
+                <li class="tooltipx">
+                    <img src="{{url('template/upscale/media/logos/white/logo-6.png')}}" alt="" />
+                </li>
+            </ul>
+        </div>
+    </section>
+
     <section class="section-base">
         <div class="container">
             <div class="row">
@@ -21,7 +143,7 @@
 
                     @endphp
                                 
-                    {{ $joblo->location_name }},
+                    {{ @$joblo->location_name }},
                             
                     {{ $job->jobs_type_time }},
 
@@ -71,6 +193,8 @@
             </div>
         </div>
     </section>    
+
+
 
     <div class="modal fade" id="mustLogin" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog" role="document">
