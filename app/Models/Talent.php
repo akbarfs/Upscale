39857f446f05378802717bda0f2f8c95fd568f51
Moch Reza <reza.upscale@gmail.com>
2022-04-28 11:59:04 +0700

    fix mod

--- app/Models/Talent.php
+++ app/Models/Talent.php
@@ -135,7 +135,7 @@ class Talent extends Model
 
 	public function user()
 	{
-		return $this->belongsTo('App\User','user_id','id');
+		return $this->belongsTo('App\Models\User','user_id','id');
 	}
 
 }
