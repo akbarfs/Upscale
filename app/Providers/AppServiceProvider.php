c6ede17a2469557203fde5cb4c829888fa0d849a
adityatriand <adityatriananda0@gmail.com>
2022-09-20 20:07:57 +0700

    change app https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,8 +16,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (config('app.env') === 'production') {
-            URL::forceScheme('https');
+        if (config('app.env') !== 'local') {
+            $this->app['request']->server->set('HTTPS', true);
         }
 
         Schema::defaultStringLength(191);
