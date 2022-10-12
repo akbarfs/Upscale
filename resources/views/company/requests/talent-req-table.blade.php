d6a8776764c871e327daa7cdc1c48dfe9dd75a69
adityatriand <adityatriananda0@gmail.com>
2022-10-12 07:32:53 +0700

    [FIX] create data talent related to request

--- resources/views/company/requests/talent-req-table.blade.php
+++ resources/views/company/requests/talent-req-table.blade.php
@@ -107,21 +107,20 @@
 
 <script>
     $(document).ready(function () {
-        var id_request = `{{ $data->company_request_id }}`;
         $('.status').on('change', function () {
             var status = this.value;
-            $.ajax({
-                url: url,
-                method: "GET",
-                data: {
-                    id_request: id_request,
-                    status: status
-                },
-                success: function (data) {
-                    $('#loading').hide();
-                    $("#talent-request").html(data);
-                }
-            });
+            // $.ajax({
+            //     url: url,
+            //     method: "GET",
+            //     data: {
+            //         id_request: id_request,
+            //         status: status
+            //     },
+            //     success: function (data) {
+            //         $('#loading').hide();
+            //         $("#talent-request").html(data);
+            //     }
+            // });
         });
     })
 </script>
